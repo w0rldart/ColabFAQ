d828c2ef683483e72ecfa898e3f20da56e851383
Alexandru Budurovici <w0rldart@w0rldart.com>
2016-01-14 15:56:45 +0000

    [FIX]  - is_authenticated() is no longer a method, but an object https://github.com/maxcountryman/flask-login/blob/master/CHANGES#L43  - logging was not being imported and thus failing in config.py
    [CHANGE]
     - changed double quotes to single ones
     - updated versions of requirements

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-import logging
+import logging, os
 from env import environment
 
 class BaseConfig(object):
--- app/factory.py
+++ app/factory.py
@@ -46,16 +46,16 @@ def create_app():
 
     @app.errorhandler(403)
     def forbidden_page(error):
-        return render_template("errors/403.html"), 403
+        return render_template('errors/403.html'), 403
 
 
     @app.errorhandler(404)
     def page_not_found(error):
-        return render_template("errors/404.html"), 404
+        return render_template('errors/404.html'), 404
 
 
     @app.errorhandler(500)
     def server_error_page(error):
-        return render_template("errors/500.html"), 500
+        return render_template('errors/500.html'), 500
 
     return app
